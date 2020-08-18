37b65b28317e097ddb6322f55cf4da0ac613a5f8
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-08-18 13:20:12 +0300

    Add files via upload
    All game states connected, tested, work as intended

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+
+{
+
+    [SerializeField] State[] nextStates;
+
+   [TextArea(10,14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
